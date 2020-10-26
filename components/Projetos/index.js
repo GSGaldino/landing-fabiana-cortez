a832961ab7c446b64d15b764fee338ca2daf3947
Gabriel <gabrielsoaresii@outlook.com>
2020-10-26 10:14:19 -0300

    Adding carousel and and title components

--- components/Projetos/index.js
+++ components/Projetos/index.js
@@ -1,11 +1,52 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
-export default function Projetos(){
+import Title from '../Title';
+
+import { IoMdArrowDropleft, IoMdArrowDropright } from 'react-icons/io';
+
+
+export default function Projetos() {
+  const [x, setX] = useState(0);
+
+  const goLeft = () => {
+    setX(x === 0 ? x - 100 : x + 100);
+  }
+
+  const goRight = () => {
+    setX(x < 0 ? x + 100 : x - 100);
+  }
+
   return (
     <div className={styles.projetos}>
-      <h2>Projetos</h2>
-      <div className="carrossel">
-        <h1>Carrossel</h1>
+      <Title text="Projetos"/>
+      <div className={styles.carousel}>
+
+        <div className={styles.slider} style={{ transform: `translateX(${x}%)` }}>
+          <div className={styles.item}>
+            <img src="/projeto1.jpg" />
+            <img src="/projeto2.jpg" />
+            <img src="/projeto2.jpg" />
+          </div>
+          <div className={styles.item}>
+            <img src="/projeto1.jpg" />
+            <img src="/projeto1.jpg" />
+            <img src="/projeto2.jpg" />
+          </div>
+        </div>
+        
+        <button onClick={goLeft} className={`${styles.carouselButton} ${styles.previous}`}>
+          <IoMdArrowDropleft
+            size={30}
+            color={"#333"}
+          />
+        </button>
+        <button onClick={goRight} className={`${styles.carouselButton} ${styles.next}`}>
+          <IoMdArrowDropright
+            size={30}
+            color={"#333"}
+          />
+        </button>
       </div>
     </div>
   )
